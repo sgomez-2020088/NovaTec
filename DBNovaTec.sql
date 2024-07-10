8e56f2a4f2b4adc4071d985327a73c1caeac7c78
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2024-07-09 18:08:05 -0600

    Se agrega entidad compras a la base de datos

---
+++ DBNovaTec.sql
@@ -0,0 +1,40 @@
+/*
+*	NovaTec
+*	IN5AM
+*  FILA 2
+*    FECHA DE CREACIÓN:
+*		05/07/2024
+*	FECHA DE MODIFICACIÓN
+*		09/07/2024
+*/
+
+drop database if exists DBNovaTec;
+create database DBNovaTec;
+
+use DBNovaTec;
+
+Create table TipoProducto (
+  codigoTipoProducto int not null auto_increment,
+  descripcion varchar(45) not null,
+  primary key PK_codigoTipoProducto (codigoTipoProducto)
+);
+
+Create table Proveedores (
+  codigoProveedor int not null auto_increment,
+  NITProveedor varchar(10) not null,
+  nombresProveedor varchar(60) not null,
+  apellidosProveedor varchar(60) not null,
+  direccionProveedor varchar(60) not null,
+  razonSocial varchar(60) not null,
+  contactoPrincipal varchar(100) not null,
+  paginaWeb varchar(50) not null,
+  primary key PK_codigoProveedor (codigoProveedor)
+);
+
+Create table Compras (
+  numeroDocumento int not null,
+  fechaDocumento date not null,
+  descripcion varchar(60) not null,
+  totalDocumento decimal(10,2) default 0.00,
+  primary key PK_numeroDocumento (numeroDocumento)
+);
